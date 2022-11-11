4784e553406929bcf770992d2e3e843246988b82
MuratGider35 <m.gider35@gmail.com>
2022-11-11 15:38:02 +0300

    details card

--- flag-app/src/pages/detail/Detail.jsx
+++ flag-app/src/pages/detail/Detail.jsx
@@ -1,41 +1,47 @@
 import "./Detail.css";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 const Detail = () => {
   const navigate = useNavigate();
   const { state: name } = useLocation();
+  console.log(name);
+
   return (
-    <div className="detailflag">
-      <h1 className="text-3xl font-bold underline">{name?.name}</h1>
-      <h1>{name.region}</h1>
-      <h1>{name.capital}</h1>
-      <h3></h3>
+    <div className="detailflag p-2">
+      <div class="card w-50 m-auto">
+        <div>
+          <img src={name?.flags.svg} class="card-img-top" alt="..."></img>
+        </div>
+
+        <div class="card-body">
+          <h5 class="card-title">{name?.name}</h5>
+          <p class="card-text">{name?.altSpellings[3]}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            {" "}
+            <strong>Capital:</strong> {name?.capital}
+          </li>
+          <li class="list-group-item">
+            {" "}
+            <strong>Region:</strong> {name?.region}
+          </li>
+          <li class="list-group-item">
+            {" "}
+            <strong>Currencies:</strong> {name?.currencies[0].code}{" "}
+            {name?.currencies[0].name}
+          </li>
+        </ul>
+        <div class="card-body text-center display-6">
+          <Link  to="/" class="card-link">
+            Home
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Detail;
-
-
-// countryDiv.innerHTML = `   <div class="card mx-auto m-3 shadow-lg" style="width: 18rem;">
-// <img src="${svg}" class="card-img-top" alt="...">
-// <div class="card-body">
-//   <h5 class="card-title">${common}</h5>
-//   <p class="card-text">${region}</p>
-// </div>
-// <ul class="list-group list-group-flush">
-//   <li class="list-group-item">
-//     <i class="fas fa-lg fa-landmark"></i> ${capital}
-//   </li>
-//   <li class="list-group-item">
-//     <i class="fas fa-lg fa-comments"></i> ${Object.values(languages)}
-//   </li>
-//   <li class="list-group-item">
-//     <i class="fas fa-lg fa-money-bill-wave"></i>
-//     ${Object.values(currencies).map((item) => Object.values(item) + " ")}
-//  </li>
-// </ul>
-
-// </div>`;
